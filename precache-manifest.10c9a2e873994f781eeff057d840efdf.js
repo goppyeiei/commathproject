776a9fa557192875dd9762e46a5313e2ea7abc5b
goppyeiei <goppyzixolo@gmail.com>
2020-09-30 10:34:57 +0700

    Updates

---
+++ precache-manifest.10c9a2e873994f781eeff057d840efdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9b35071990b0392a11642adb67e8eb",
+    "url": "/commathproject/index.html"
+  },
+  {
+    "revision": "b830285c318ebb6eff5a",
+    "url": "/commathproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44efbeb1a345ba240663",
+    "url": "/commathproject/static/js/2.6b633340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commathproject/static/js/2.6b633340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b830285c318ebb6eff5a",
+    "url": "/commathproject/static/js/main.6be01cbd.chunk.js"
+  },
+  {
+    "revision": "d42f99306f36200df8d6",
+    "url": "/commathproject/static/js/runtime-main.dd14d573.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commathproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
